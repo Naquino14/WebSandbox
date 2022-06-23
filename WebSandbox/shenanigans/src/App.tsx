9c928a21bb2af1cad616d283f8379a8e41971e45
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-06-23 13:35:32 -0400

    introductory shenanigans

--- WebSandbox/shenanigans/src/App.tsx
+++ WebSandbox/shenanigans/src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+console.clear()
+
+const name = "nate"
+let guhh:[boolean, string] = [true, name]
+
+let zooweemama: meme
+zooweemama = {
+  name: "blah"
+}
+let weewee : meme = { 
+  name: name,
+  funny: 10 
+  }
+
+type meme = {
+  name: string,
+  funny?: number | string
+}
+
+let myfunnies: meme[] = [
+  zooweemama, weewee, {name: "krabber patty", funny: "not funny???"}
+]
+console.log("this is a funny interpolated string and it uses \` so cool i think")
+
+//let printFunny : (funny: meme | meme[] ) => void
+
+let printFunny = (funny: meme | meme[]) =>
+{
+  if (funny instanceof Array)
+  {
+    funny.forEach(e => 
+    { 
+      console.log(`In your list of funnies, here is a funny: 
+      ${e.name}, 
+      ${e.funny ?? "no funny"}. 
+      The funny score is a ${typeof(e.funny)}`) 
+    })
+  }
+  else
+  {
+    console.log(`Here is the funny: 
+        ${funny.name}, 
+        ${funny.funny ?? "no funns"}. 
+        The funny score is a ${typeof(funny.funny)})}`)
+  }
+}
+console.log(typeof weewee)
+console.log(typeof myfunnies)
+
+printFunny(myfunnies)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App">Hello World!</div>
+  )
 }
 
-export default App;
+export default App
