cb8515e1e90846349378e89c512b651359193a7e
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-07-02 18:35:51 -0400

    new api and work on shenanigans
    prepare to merge!

--- WebSandbox/shenanigans/src/App.tsx
+++ WebSandbox/shenanigans/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 import Foot from './components/Foot'
+import FunnyList from './components/FunnyList'
+import { Funny } from './components/FunnyModel'
 import InputField from './components/InputField'
 import './GlobalFonts.css'
 
@@ -8,15 +10,29 @@ import './GlobalFonts.css'
 const App: React.FC = () => {
   // Now im gonna define a state by setting both of these variables?
   // this is a basic "hook"
-  const [funny, setFunny] = React.useState<string>('');
-  
+  const [funny, setFunny] = React.useState<string>('')
+  const [funnies, setFunnies] = React.useState<Funny[]>([])
+
+  const addFunny = (e : React.FormEvent) => {
+    e.preventDefault();
+
+    if (funny)
+    {
+      setFunnies([...funnies, {id: funnies.length, funny: funny, isCringe: false}])
+      setFunny('')
+    }
+  }
+
+  console.log(funnies)
+
   return (
     <div className="App">
       <span className="heading">Funnyify</span>
-      <InputField funny={funny} setFunny={setFunny}/> 
+      <InputField funny={funny} setFunny={setFunny} addFunny={addFunny}/> 
       {/* ok so this is really cool. 
       Its a JSX element defined in its 
       sown file and can be reused anywhere! */}
+      <FunnyList funnies={funnies} setFunnies={setFunnies}/>
       <Foot/>
     </div>
     
---
+++ WebSandbox/shenanigans/src/components/FunnyModel.ts
@@ -0,0 +1,6 @@
+export interface Funny
+{
+    id: number
+    funny: string
+    isCringe: boolean
+}
